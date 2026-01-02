d51f7ec65a9926e5eebe2ec138848a9b1d264e6f
Raul <theRaulc@gmail.com>
2026-01-03 00:22:36 +0100

    Fix Tailwind and update home to watercolor flyer layout

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
